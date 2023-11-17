fe29019de4667fac19228342a4d2803596ccc948
caiovtr <caiovitorandraderosa@gmail.com>
2023-11-17 10:42:42 -0300

    o retorno 3.0

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,21 @@
 import * as S from "./styles"
 import Logo from "../../assets/logovasco.png"
+import { Link, useNavigate } from "react-router-dom";
+import { GrLogout } from "react-icons/gr";
+import { useContext } from "react";
+import { AuthContext } from "../../contexts/authContext";
 
 export function Header() {
+  const { user, signOut } = useContext(AuthContext)
+  const navigate = useNavigate();
+  async function logout() {
+    await signOut()
+    navigate('/login')
+  }
   return (
     <S.Header>
       <picture>
-          <img src={Logo} alt="Logo Vasco" />
+        <img src={Logo} alt="Logo Vasco" />
       </picture>
       <S.NavBar>
         <ul>
@@ -16,6 +26,24 @@ export function Header() {
             <a href="/list">Notícias</a>
           </li>
         </ul>
+        {
+          user ? (
+            <ul>
+              <li>
+                <button onClick={logout}>{user.name} <GrLogout /></button>
+              </li>
+            </ul>
+          ) : (
+            <ul>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+              <li>
+                <Link to="/cadastrar">Cadastrar</Link>
+              </li>
+            </ul>
+          )
+        }
       </S.NavBar>
     </S.Header>
   )
